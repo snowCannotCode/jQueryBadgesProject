513239c178c84dcfb48c8ec07a1e68f3b510afbc
Snow <1558538178@qq.com>
2017-01-26 10:59:26 +0800

    1

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
 $(function() {
+  $.ajax{
+    url:'https://www.codeschool.com/users/snowCannotCode.json',
+    dataType:'jsonp',
+    success:function(response){
+      var badges=$('#badges');
+      for(var i=0,len=response.courses.completed.length;i<len;i++){
+        var h3=$('<h3></h3>');
+        var div=$('<div></div>');
+        h3.appendTo(div);
+        div.addClass('course');
+        div.appendTo(badges);
+      }
+    }
+  }
 
-  // your code will go here
 
 });
